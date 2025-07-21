d54af7da867c2b5daa0f67b6ffbae0db21877a1f
Mohammad Hasan Atashbar <mohammadgc.01@gmail.com>
2025-07-21 21:08:53 +0330

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,41 +6,41 @@ const Permissions = require('../constants/Permission');
 const { get_logs, delete_log, download_log } = require("../controller/log_controller");
 const { checkPermission } = require("../middleware/checkPermission");
 
-router.get('/', (req , res) => {
-    res.render('home')
+router.get('/', (req, res) => {
+  res.render('home')
 })
 
 
 
-router.get('/images/:name', (req , res) => {
-   const image_path = path.resolve(__dirname , '../public/images')
-   const images = fs.readdirSync(image_path)
-   const result = images.find(file => file.startsWith(req.params.name + "."))
-    const full_path = path.resolve(`public/images/${result}`) 
-    res.sendFile(full_path)
+router.get('/images/:name', (req, res) => {
+  const image_path = path.resolve(__dirname, '../public/images')
+  const images = fs.readdirSync(image_path)
+  const result = images.find(file => file.startsWith(req.params.name + "."))
+  const full_path = path.resolve(`public/images/${result}`)
+  res.sendFile(full_path)
 })
 
 
-router.get('/log/get', authentication, async (req , res) => {
+router.get('/log/get', authentication, async (req, res) => {
   const user = await authorization(req);
-  const canAccess = await checkPermission(user.roles,Permissions.VIEW_LOGS);
+  const canAccess = await checkPermission(user.roles, Permissions.VIEW_LOGS);
   if (!canAccess) return res.status(403).json({ message: "You do not have permission to perform this action" });
-  get_logs(req , res)
+  get_logs(req, res)
 })
 
-router.post('/log/delete/:id', async (req , res) => {
-   const user = await authorization(req);
-  const canAccess = await checkPermission(user.roles,Permissions.VIEW_LOGS);
+router.post('/log/delete/:id', async (req, res) => {
+  const user = await authorization(req);
+  const canAccess = await checkPermission(user.roles, Permissions.VIEW_LOGS);
   if (!canAccess) return res.status(403).json({ message: "You do not have permission to perform this action" });
-  delete_log(req , res)
+  delete_log(req, res)
 })
 
-router.get('/log/download/all', authentication ,async (req , res) => {
- const user = await authorization(req);
-  const canAccess = await checkPermission(user.roles,Permissions.VIEW_LOGS);
+router.get('/log/download/all', authentication, async (req, res) => {
+  const user = await authorization(req);
+  const canAccess = await checkPermission(user.roles, Permissions.VIEW_LOGS);
   if (!canAccess) return res.status(403).json({ message: "You do not have permission to perform this action" });
-  
-  download_log(req , res)
+
+  download_log(req, res)
 })
 
 module.exports = router
